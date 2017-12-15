20ccc3580e9ec2edd2253cb7ad9b17175266ed1c
abhigupta768 <abhigupta768@github.com>
2017-12-15 17:55:46 +0530

    Removed unnecessary files

--- your_shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import category, item, delivery
-
-admin.site.register(category)
-admin.site.register(item)
-admin.site.register(delivery)
-# Register your models here.
